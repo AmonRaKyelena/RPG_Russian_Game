599dc9d86668da99cbff72b911674d6036296691
Aleksei <xxxleshaxxx4@gmail.com>
2024-02-18 00:16:42 +0300

    Initial commit

---
+++ Source/ProgectWithLesha/ProgectWithLeshaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProgectWithLeshaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGECTWITHLESHA_API AProgectWithLeshaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
